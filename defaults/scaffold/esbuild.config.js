86c27eac19fabca598dc216bffe2f97b5af0837a
wkoehn <wade@koehndesign.com>
2021-03-20 11:57:37 -0600

    fixed default esbuild config and added examples

--- defaults/scaffold/esbuild.config.js
+++ defaults/scaffold/esbuild.config.js
@@ -1,3 +1,16 @@
-module.exports = () => ({
-  plugins: [],
-});
+// for svelte - tested and currently working
+// const svelte = require('esbuild-svelte');
+
+// for vue 2
+// https://github.com/apeschar/esbuild-vue
+
+// for react
+// https://github.com/evanw/esbuild/issues/7
+
+module.exports = {
+  plugins: [
+    // required format for esbuild
+    // see https://github.com/esbuild/community-plugins#how-to-use-a-plugin
+    // svelte(),
+  ],
+};
