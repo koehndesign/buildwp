246cf39238492f482d83856c3a05d6c01b64030b
wkoehn <wade@koehndesign.com>
2021-04-21 11:53:01 -0600

    added specific require to tasks

--- bin/buildwp.js
+++ bin/buildwp.js
@@ -1,26 +1,23 @@
 #!/usr/bin/env node
-const { dev, prod, release } = require('./tasks');
-const { setup } = require('./setup');
-
 const argv = require('yargs/yargs')(process.argv.slice(2))
   .command({
     command: 'setup',
     desc: 'scaffold a BuildWP project',
-    handler: (argv) => setup(argv),
+    handler: (argv) => require('./setup').setup(argv),
   })
   .command({
     command: 'dev [dest]',
     desc: 'build for development to --dest and watch files for changes',
-    handler: (argv) => dev(argv),
+    handler: (argv) => require('./tasks').dev(argv),
   })
   .command({
     command: 'prod [dest]',
     desc: 'build for production to --dest',
-    handler: (argv) => prod(argv),
+    handler: (argv) => require('./tasks').prod(argv),
   })
   .command({
     command: 'release',
     desc: 'build for production and zip for WP release',
-    handler: (argv) => release(argv),
+    handler: (argv) => require('./tasks').release(argv),
   })
   .help().argv;
